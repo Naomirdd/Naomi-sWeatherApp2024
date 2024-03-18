5e3463bb837c21625fc4d1203c4d7b55e4ad6273
Naomirdd <riddnaomi1@gmail.com>
2024-03-18 16:50:04 +0000

    Tidied up Java

--- src/index.js
+++ src/index.js
@@ -3,35 +3,39 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  let humditiyElement=document.querySelector("#humdity");
-  let windSpeedElement=document.querySelector("#wind-speed");
-  let timeElement=document.querySelector("#time");
-  let date = new Date (response.data.time•1000);
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-
-
-descriptionElement.innerHTML=response.data.condition.description;
   cityElement.innerHTML = response.data.city;
-  humditiyElement.innerHTML= `${response.data.temperature.humidity}%`;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-  windElement.innerHTML= `${response.data.wind.speed}%`  
-  timeElement.innterHTML = formatDate(date);
-
-function FormateDate(date){
-
-  if (minutes < 10 ){
-    minutes = '0${minutes}' ; 
-  }
-  
-  let minutes = date.getMinutes();
-  let hours= date.getHours();
-let days =[ "Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]; 
-return "Tuesday 18:48 ";
-let day = days [date.getday()];
-return '${day} ${hours} :${ minutes}';
-
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
